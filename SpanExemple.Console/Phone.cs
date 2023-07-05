fedf4e4c4c728dad0d492206857be88c35adf3c5
Gabriel Faria <gggfaria@outlook.com>
2023-07-05 14:30:29 -0300

    Add project files.

---
+++ SpanExemple.Console/Phone.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpanExemple.Console
+{
+    [MemoryDiagnoser]
+    public class Phone
+    {
+        private static readonly string _phoneString = "+5524955556342";
+
+
+        [Benchmark]
+        public string GetAreaCode()
+        {
+            var areaCode = _phoneString.Substring(3, 2);
+            return areaCode;
+        }
+
+        [Benchmark]
+        public ReadOnlySpan<char> GetAreaCodeSpan()
+        {
+            ReadOnlySpan<char> phoneSpan = _phoneString;
+            var areaCode = phoneSpan.Slice(3, 2);
+            return areaCode;
+        }
+
+    }
+}
---
+++ SpanExemple.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using SpanExemple.Console;
+
+
+var phone = new Phone();
+Console.WriteLine(phone.GetAreaCodeSpan().ToString());
+
+BenchmarkRunner.Run<Phone>();
